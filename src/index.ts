2cb10588f81819ca4c8fc523c7dac98a7c103c96
Maxim Tovstashev <maxim.tovstashev@join.com>
2019-06-13 14:00:36 +0200

    Env func (#1)
    * evn() function added
    
    * updated packages

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,41 @@ export const processEnv = (envVarName: string): string => {
 };
 
 export const processEnvInt = (envVarName: string): number => {
-  const env = processEnv(envVarName);
-  return parseInt(env, 10);
+  const variable = processEnv(envVarName);
+  return parseInt(variable, 10);
+};
+
+const asInt = (str: string | undefined, varName: string): number => {
+  if (!str) {
+    return 0;
+  }
+  const num = parseInt(str, 10);
+  if (Number.isNaN(num)) {
+    throw new Error(`env.${varName} is expected to be number`);
+  }
+  return num;
+};
+
+const asBool = (str: string | undefined, varName: string): boolean => {
+  if (!str) {
+    return false;
+  }
+  if (str !== 'true' && str !== 'false') {
+    throw new Error(
+      `env.${varName} is expected to be either "true" or "false"`,
+    );
+  }
+  return str === 'true';
+};
+
+export const env = (varName: string, optional = false) => {
+  const variable = process.env[varName];
+  if (!variable && !optional) {
+    throw new Error(`env.${varName} is not set`);
+  }
+  return {
+    asString: () => variable || '',
+    asInt: () => asInt(variable, varName),
+    asBool: () => asBool(variable, varName),
+  };
 };
