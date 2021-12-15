611d80da7095809555b3d7481248b303d0d3bfa6
Adam Misiorny <adam.misiorny@gmail.com>
2021-12-15 15:13:40 +0100

    remove lib from package

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const processEnv: (envVarName: string) => string;
-export declare const processEnvInt: (envVarName: string) => number;
-export declare const env: (varName: string, optional?: boolean) => {
-    asString: () => string;
-    asInt: () => number;
-    asNumber: () => number;
-    asBool: () => boolean;
-};
